46377a9c5f7d21e00d7112417f2d0ae4847f2992
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-07 21:53:04 +0600

    fetch single user data

--- Django_ORM/orm/views.py
+++ Django_ORM/orm/views.py
@@ -4,5 +4,9 @@ from .models import *
 def home(request):
 
     #Fetch all user data
-    users =User.objects.all().values()
-    return JsonResponse({"user":list(users)})
+    # users =User.objects.all().values()
+    # return JsonResponse({"user":list(users)})
+
+    #Fetch single user data
+    users =User.objects.get(id=1)
+    return JsonResponse({"user":(users.username)})
